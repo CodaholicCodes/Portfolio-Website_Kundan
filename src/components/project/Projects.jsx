373635a8e9c99c874b7324bff6ee446bfe6ae41c
Kundan Singh <kundansingh68788@gmail.com>
2025-10-28 17:24:59 +0530

    Demo Url Changes

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -7,26 +7,26 @@ const Projects = () => {
       title: "Airbnb Clone",
       desc: "Developed a full-stack Airbnb-inspired platform featuring property listings, user authentication, booking functionality, and database integration.",
       techUsed: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
-      demoUrl : "https://airbnb-4r1m.onrender.com",
+      demo: "https://airbnb-4r1m.onrender.com",
 
     },
     {
       title: "Currency Converter",
       desc: "Built a real-time currency converter that fetches live exchange rates through API integration.",
       techUsed: ["React", "Tailwind CSS", "REST API", "Next JS"],
-      demoUrl : "https://currencyconverter-frontend-1opb.onrender.com",
+      demo: "https://currencyconverter-frontend-1opb.onrender.com",
     },
       {
       title: "Spam Detection App",
       desc: "Created a spam message detection system using a static dataset of 100 messages to classify spam and non-spam content.",
       techUsed: ["React", "Node.js", "Express", "Tailwind CSS"],
-      demoUrl : "https://spam-detector-frontend-4h9c.onrender.com/"
+      demo : "https://spam-detector-frontend-4h9c.onrender.com/"
     },
   ];
   return (<Section icon={<Briefcase />} sectionHeading={"Projects"}>
 {
 projectsList.map((item) => 
-  <Project title={item.title} desc={item.desc} techStack={item.techUsed} demoUrl={demoUrl}  />
+  <Project title={item.title} desc={item.desc} techStack={item.techUsed} demoUrl={item.demo}  />
 )
 }
 </Section>);
