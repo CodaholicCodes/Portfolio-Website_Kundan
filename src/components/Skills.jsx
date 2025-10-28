8a85d5ae3a8c695334724bd625113f036f092cb9
Kundan Singh <kundansingh68788@gmail.com>
2025-10-28 20:19:40 +0530

    SKills Changed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import { Code  } from 'lucide-react';
 import Section from './Section.jsx';
 import Pills from './skillPills.jsx';
 const Skills=()=>{
-        const skills=['Java','C','HTML','CSS','JavaScript','React JS','Git'];
+        const skills=['Java','HTML','CSS','React JS','Git','Node Js','Express Js','MONGO DB'];
 return <Section icon={<Code  />} sectionHeading="Skills">
    { skills.map(title=> <Pills  key={title} title={title}/>)}
 
